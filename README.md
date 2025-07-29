a375b45bfba64deb8f49051e741ee7e6f69cc0bf
Tejesh Babu M V <tejeshbabu25@gmail.com>
2025-07-28 20:06:23 -0500

    Adding DbContext and DBSets

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DotnetCore_API_EF_SQL_Authen_Author
 Use C# and Build an ASP.NET Core Web API with Entity Framework Core, SQL Server, Authentication, Authorization | .NET8
+
+# DbContext Class
+- Maintaining Connection to database
+- Track Changes
+- Perform CRUD Operations
+- Acts as a bridge between domain models and the database
---
+++ USTrails_API/USTrails/USTrails.API/Data/USTrailsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using USTrails.API.Models.Domain;
+
+namespace USTrails.API.Data
+{
+    public class USTrailsDbContext : DbContext
+    {
+        public USTrailsDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+
+        // DBSet is a property of dbcontext class that represents a collection of entities in the database
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Trail> Trails { get; set; }
+
+    }
+}
